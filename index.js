c1c4a63642c3012163214c24422017893a4226b9
davideliason <davethemaker@gmail.com>
2017-07-17 08:36:00 -0700

    install express dotenv pkgs and create simple express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express		= require('express');
+const path 			= require('path');
+
+const app 			= express();
+const port 			= process.env.PORT || 5000;
+// require('dotenv').config();
+
+// app.use(express.static(path.join(__dirname,'/public')));
+
+app.get('/sample', (req,res) => {
+	res.send('i am a sample');
+});
+
+app.listen(port);
+console.log(`server up at port ${port}`);
