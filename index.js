364c9a8f9fce62cb5df2c475f6992eff38ee32ef
davideliason <davethemaker@gmail.com>
2017-07-17 10:04:34 -0700

    successfully connect with mLab mongodb instance of dailydrinks database and console.log first stored objects drink value

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const express		= require('express');
 const path 			= require('path');
 
 const app 			= express();
-const MongoClient   = require('mongodb').MongoClient;
+const mongodb  	    = require('mongodb');
+const MongoClient   = mongodb.MongoClient;
+
+require('dotenv').config();
+
 
 const port 			= process.env.PORT || 5000;
 const drinks 		= require('./drinks.js');
 
-require('dotenv').config();
 
-MongoClient.connect(process.env.MONGOLAB_URI, (err,database) => {
+app.use(express.static(path.join(__dirname,'/public')));
+
+
+MongoClient.connect( process.env.MONGOLAB_URI, (err,database) => {
 	if(err) return console.log(err)
 	console.log('connected to mLab db');
-	db = database;
 
-	app.use(express.static(path.join(__dirname,'/public')));
+    db = database;
+
+
+	app.get('/drinklog', (req,res) => {
+		db.collection('drinklog').find().toArray(function(err,result){
+			if(err)return console.log(err)
+			console.log("results here:" + result[0].drink);
+		});
+	});
+
 	// use Router
 	app.use('/drinks', drinks);
 
@@ -29,6 +43,8 @@ MongoClient.connect(process.env.MONGOLAB_URI, (err,database) => {
 	app.get('/samplePostForm', (req,res) => {
 		res.sendFile(path.join(__dirname,'/public','/index.html'));
 	});
+
+	
 
 
 
